8c79df0cd26d26811dd4c10aed69c1837f8b35da
Lucas R <lucasrenandra@gmail.com>
2025-10-22 20:26:25 +0700

    hmm

--- test_login.py
+++ test_login.py
@@ -5,11 +5,12 @@ def test_main() -> None :
         ff_browser = p.firefox.launch()
         page = ff_browser.new_page()
         page.goto("https://the-internet.herokuapp.com/login")
-        expect(page.get_by_role("heading", name='Login Page')).to_be_visible() 
+        expect(page.get_by_role("heading", name='Login Page')).to_be_visible()
         ff_browser.close()
         
     
     
 
 if __name__  == "__main__" : 
-    test_main()
+    # test_main()
+    ...
---
+++ test_valid_login.py
@@ -0,0 +1,5 @@
+# from playwright.sync_api import Page, expect 
+
+
+# def test_valid_login() -> None :
+#     login = Page("")
