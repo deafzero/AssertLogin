8f57c2fd434aed1bfec41e0b498324177b9357f0
Lucas R <lucasrenandra@gmail.com>
2025-10-23 10:17:01 +0700

    Done!

--- test_login.py
+++ test_login.py
@@ -1,13 +1,57 @@
-from playwright.sync_api import sync_playwright, expect
+from playwright.sync_api import Page, expect
 
-def test_main() -> None : 
-    with sync_playwright() as p :
-        ff_browser = p.firefox.launch()
-        page = ff_browser.new_page()
-        page.goto("https://the-internet.herokuapp.com/login")
-        expect(page.get_by_role("heading", name='Login Page')).to_be_visible()
-        ff_browser.close()
+
+URL = "https://the-internet.herokuapp.com/login"
+
+def test_is_page_login(page: Page) -> None : 
+    page.goto(URL)
+    expect(page.get_by_role("heading", name="Login Page")).to_be_visible()
+
+# Positive test case valid login with username and password true 
+def test_valid_login(page: Page)  -> None : 
+    page.goto(URL)
+    page.get_by_label("Username").fill("tomsmith")
+    page.get_by_label("Password").fill("SuperSecretPassword!")
+    page.locator("html.no-js body div.row div#content.large-12.columns div.example form#login button.radius").click()
+    expect(page.get_by_role("heading", name="Secure Area", exact=True)).to_be_visible()
+
+def test_wrong_password(page: Page) -> None:
+    page.goto(URL)
+    page.get_by_label("Username").fill("tomsmith")
+    page.get_by_label("Password").fill("Falsepassword")
+    page.locator("html.no-js body div.row div#content.large-12.columns div.example form#login button.radius").click()
+
+    # Pass jika tidak berhasil login ke secure AREA
+    expect(page.locator('#flash-messages')).to_contain_text("Your password is invalid! ")
         
+def test_wrong_username(page: Page) -> None:
+    page.goto(URL)
+    page.get_by_label("Username").fill("Falsetomsmith")
+    page.get_by_label("Password").fill("SuperSecretPassword!")
+    page.locator("html.no-js body div.row div#content.large-12.columns div.example form#login button.radius").click()
+
+    # Pass jika tidak berhasil login ke secure AREA
+    expect(page.locator('#flash-messages')).to_contain_text("Your username is invalid! ")
+
+    
+def test_case_sensitive_username(page: Page) -> None :
+    page.goto(URL)
+    page.get_by_label("Username").fill("TOMSMITH")
+    page.get_by_label("Password").fill("SuperSecretPassword!")
+    page.locator("html.no-js body div.row div#content.large-12.columns div.example form#login button.radius").click()
+
+    # Pass jika tidak berhasil login ke secure AREA
+    expect(page.locator('#flash-messages')).to_contain_text("Your username is invalid! ")
+
+def test_case_sensitive_password(page: Page) -> None :
+    page.goto(URL)
+    page.get_by_label("Username").fill("tomsmith")
+    page.get_by_label("Password").fill("SUPERSECRETPASSWORD!")
+    page.locator("html.no-js body div.row div#content.large-12.columns div.example form#login button.radius").click()
+
+    # Pass jika tidak berhasil login ke secure AREA
+    expect(page.locator('#flash-messages')).to_contain_text("Your password is invalid! ")
+
     
     
 
--- test_valid_login.py
+++
@@ -1,5 +0,0 @@
-# from playwright.sync_api import Page, expect 
-
-
-# def test_valid_login() -> None :
-#     login = Page("")
